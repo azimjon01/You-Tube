cfb9e0b06fe7bb82880c9e400044aa909d1b760b
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-02-01 13:11:25 +0500

    Birinchi commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { CategoryPills } from "./components/CategoryPills";
+import { PageHeader } from "./layouts/PageHeader";
+
+function App() {
+  return (
+    <div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+        <div>Sidebar</div>
+        <div className="sticky top-0 bg-white z-10 pb-4">
+          <CategoryPills />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

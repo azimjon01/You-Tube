d614f56bf92f133089593002130e7a4f281c4dc3
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-02-02 11:57:04 +0500

    O'ng va chap tomonga harakatlantiradigan navbardagi tugmalarni to'g'irladim

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <PageHeader />
       <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
         <div>Sidebar</div>
-        <div className="overflow-x-hidden">
+        <div className="overflow-x-hidden px-8 pb-4">
           <div className="sticky top-0 bg-white z-10 pb-4">
             <CategoryPills
               categories={categories}
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,4 +1,6 @@
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./Button";
+import { useState } from "react";
 
 type CategoryPillProps = {
   categories: string[];
@@ -11,6 +13,8 @@ export function CategoryPills({
   selectedCategory,
   onSelect,
 }: CategoryPillProps) {
+  const [isLeftVisible, setIsLeftVisible] = useState(false);
+  const [isRightVisible, setIsRightVisible] = useState(true);
   return (
     <div className="overflow-x-hidden relative">
       <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
@@ -25,6 +29,28 @@ export function CategoryPills({
           </Button>
         ))}
       </div>
+      {isLeftVisible && (
+        <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronLeft />
+          </Button>
+        </div>
+      )}
+      {isRightVisible && (
+        <div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-24 h-full flex justify-end">
+          <Button
+            variant="ghost"
+            size="icon"
+            className="h-full aspect-square w-auto p-1.5"
+          >
+            <ChevronRight />
+          </Button>
+        </div>
+      )}
     </div>
   );
 }

f18ce6a010ceb7adb960366a459cb02394f8c543
Azimjon Mamatqulov <mamatqulovazimjon19@gmail.com>
2025-02-02 11:31:43 +0500

    o'zgarish

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -2,21 +2,28 @@ import { Button } from "./Button";
 
 type CategoryPillProps = {
   categories: string[];
+  selectedCategory: string;
+  onSelect: (category: string) => void;
 };
 
-export function CategoryPills({ categories }: CategoryPillProps) {
+export function CategoryPills({
+  categories,
+  selectedCategory,
+  onSelect,
+}: CategoryPillProps) {
   return (
     <div className="overflow-x-hidden relative">
       <div className="flex whitespace-nowrap gap-3 transition-transform w-[max-content]">
-        <Button
-          variant="dark"
-          className="py-1 px-3 rounded-lg whitespace-nowrap"
-        >
-          All
-        </Button>
-        <Button className="py-1 px-3 rounded-lg whitespace-nowrap">
-          JavaScript
-        </Button>
+        {categories.map((category) => (
+          <Button
+            key={category}
+            onClick={() => onSelect(category)}
+            variant={selectedCategory === category ? "dark" : "dafault"}
+            className="py-1 px-3 rounded-lg whitespace-nowrap"
+          >
+            {category}
+          </Button>
+        ))}
       </div>
     </div>
   );
